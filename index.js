64109ddb6fa4404d9f3375896c57be0dc7bead71
Ann <nuraann13@gmail.com>
2021-12-22 16:49:08 +0000

    first commt

---
+++ index.js
@@ -0,0 +1,20 @@
+
+const bodyParser = require('body-parser')
+const express = require('express')
+const app = express()
+app.use(bodyParser.urlencoded())
+
+const port = process.env.PORT || 8080
+
+app.get('/', (req, res) =>
+{
+  res.render('index.ejs')
+})
+app.post('/', (req, res) => {
+  const username = req.body.username
+  res.render('hello.ejs', {username: username})
+})
+
+app.listen(port, () => {
+  console.log(`listen port:${port}`)
+})
